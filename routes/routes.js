6e254b251150463ba32e24658be84a5235c4e9ba
angelia collins <angiecollins09@gmail.com>
2021-02-12 20:36:16 -0600

    attempting some routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,21 @@
 const path = require("path");
+console.log("howdy");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  console.log("howdy2");
 
-    app.get("/exercise", (req, res) => {
-        res.render('exercise');
-    
-      });
+  app.get("/", (req, res) => {    console.log("howdy/");
 
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/exercise", (req, res) => {
+    console.log("howdy");
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+console.log("yyee");
 };
